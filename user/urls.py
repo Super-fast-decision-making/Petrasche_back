5734c4a03f8105abf0c24c9510b1ffa8afacabb9
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-09 11:01:31 +0900

    add:비밀번호 해싱

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ from user import views
 
 urlpatterns = [
     path('', views.UserView.as_view()),
+    path('login/', views.TokenObtainPairView.as_view(), name='patrasche_token'),
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from .serializers_jwt import TokenObtainPairSerializer
 from .serializers import UserSerializer
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 #회원가입
 class UserView(APIView):
@@ -12,7 +14,11 @@ class UserView(APIView):
 
     def post(self,request):
         user_serializer=UserSerializer(data=request.data)
-
         if user_serializer.is_valid():
             user_serializer.save()
+            # return Response({"msg:회원가입 성공"})
             return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+# 로그인
+class TokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
