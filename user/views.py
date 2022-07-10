781eacebabb4489302b1245f9fb613f0ed5e55ed
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-10 21:39:21 +0900

    refac:팔로우/언팔 리팩토링

--- user/views.py
+++ user/views.py
@@ -8,43 +8,51 @@ from .serializers import UserSerializer
 from .models import User, UserFollowing
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 
 class UserView(APIView):
     permission_classes = [permissions.AllowAny]
-    # 사용자 정보 조회
-    def get(self, request):
-        print("1:", request.user)
-        user_serializer = UserSerializer(request.user).data
-        user_serializer['followers'] = UserFollowing.objects.filter(following_user_id=request.user).count() # 나를 팔로우 하는 사람 수
-        user_serializer['followings'] = UserFollowing.objects.filter(user_id=request.user).count() # 내가 팔로우 하는 사람 수
-        print("2:", user_serializer)
-        return Response(user_serializer, status=status.HTTP_200_OK)
 
     # 회원가입
     def post(self, request):
         user_serializer=UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
-
             return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # 로그인 기능
 class TokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
 
 
-# 팔로잉
+class OnlyAuthenticatedUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes=[JWTAuthentication]
+
+    # 사용자 정보 조회
+    def get(self, request):
+        if request. user:
+            user_serializer = UserSerializer(request.user).data
+            user_serializer['followers'] = UserFollowing.objects.filter(following_user_id=request.user).count() # 나를 팔로우 하는 사람 수
+            user_serializer['followings'] = UserFollowing.objects.filter(user_id=request.user).count() # 내가 팔로우 하는 사람 수
+            return Response(user_serializer, status=status.HTTP_200_OK)
+        return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+# 팔로우/언팔로우
 class UserFollowingView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes=[JWTAuthentication]
+
     def post(self,request):
         following_user=User.objects.get(username=request.data['username'])
-        UserFollowing.objects.create(user_id=request.user, following_user_id= following_user)
-        return Response({'msg:성공'}, status=status.HTTP_200_OK)
+        new_follow, created = UserFollowing.objects.get_or_create(user_id=request.user, following_user_id= following_user)
+        if created:
+            new_follow.save()
+            return Response({"message": "팔로우 성공!"}, status=status.HTTP_200_OK)
+        new_follow.delete()
+        return Response({"message":"언.팔.로.우"}, status=status.HTTP_200_OK)
 
-    #언팔로우
-    def delete(self,request):
-        following_user=User.objects.get(username=request.data['username'])
-        UserFollowing.objects.filter(user_id=request.user, following_user_id=following_user).delete()
-        return Response({'message': '삭제되었습니다'}, status=status.HTTP_200_OK)
