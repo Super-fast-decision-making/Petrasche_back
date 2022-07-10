5f7d78894a8bca395fa0f601921870c5f807cf7e
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-10 13:06:54 +0900

    머지

--- user/models.py
+++ user/models.py
@@ -72,3 +72,9 @@ class User(BaseModel, AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
