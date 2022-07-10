582d71a94b6c599209f15f95531a52a8a77d370f
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-10 21:57:04 +0900

    add: 유저 수정api

--- user/views.py
+++ user/views.py
@@ -26,7 +26,7 @@ class UserView(APIView):
 class TokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
 
-# 사용자 정보 조회
+# 사용자 정보 조회//수정
 class OnlyAuthenticatedUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes=[JWTAuthentication]
@@ -38,6 +38,16 @@ class OnlyAuthenticatedUserView(APIView):
             user_serializer['followings'] = UserFollowing.objects.filter(user_id=request.user).count() # 내가 팔로우 하는 사람 수
             return Response(user_serializer, status=status.HTTP_200_OK)
         return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def put(self, request, obj_id):
+        user = User.objects.get(id=obj_id)
+        if request.user != user:
+            return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+        user_serializer = UserSerializer(user, request.data, partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 팔로우/언팔로우
