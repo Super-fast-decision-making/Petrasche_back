bbc9d8d2c965e911b164f40a7bd7c861ae80d350
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-11 01:08:00 +0900

    add: 회원가입 validation 주석으로 추가해둠

--- user/serializers.py
+++ user/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from .models import User, UserFollowing
 
+EMAIL = ("@naver.com", "@gmail.com", "@kakao.com")
 
-# class UserFollowingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=UserFollowing
-#         fields=['user_id', 'following_user_id', 'created_at']
+
 
 class UserSerializer(serializers.ModelSerializer):
+
+#     def validate(self, data):
+#         if not len(data.get("username", "")) >= 6:
+#             raise serializers.ValidationError(
+#                 detail={"error": "username의 길이는 6자리 이상이어야 합니다."}
+#             )
+#         if not data.get("email", "").endswith(EMAIL):
+#             raise serializers.ValidationError(
+#                 detail={"error": "네이버, 구글, 카카오 이메일만 가입할 수 있습니다."}
+#             )
+#         if not len(data.get("password", "")) >= 6:
+#             raise serializers.ValidationError(
+#                 detail={"error": "password의 길이는 6자리 이상이어야 합니다."}
+            # )
 
     def create(self, validated_data):
         password = validated_data.pop("password")
