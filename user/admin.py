3b72512463c52e62e6ffd465930f3a1ed7b2f5ec
앵가리 <>
2022-07-11 16:11:20 +0900

    feat: 반려동물 프로필 모델 생성

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from user.models import User, UserFollowing
+from user.models import User, UserFollowing, PetProfile
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserFollowing)
+admin.site.register(PetProfile)
