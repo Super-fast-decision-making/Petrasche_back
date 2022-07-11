0328d9b2045f5247c5d6d67b41a7d02b257284a5
min_fs <103223878+mingoodjob@users.noreply.github.com>
2022-07-11 10:02:25 +0900

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.UserView.as_view()),
     path('login/', views.TokenObtainPairView.as_view(), name='patrasche_token'),
     path('follow/', views.UserFollowingView.as_view()),
-    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
