31f31c0d9bb0a74eb204a47aa660e2864efc947a
minmincoding <psjlmk@gmail.com>
2022-07-16 15:08:40 +0900

    add 유저 어드민 커스텀

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 from user.models import User, UserFollowing, UserProfile, PetProfile
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(UserFollowing)
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    extra = 1
 
-admin.site.register(PetProfile)
+class PetProfileInline(admin.StackedInline):
+    model = PetProfile
+    extra = 1
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','email', 'username',)
+    list_display_links = ('email', 'username',)
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('email', 'username',)
+    ordering = ('-created_at',)
+    list_per_page = 10
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ("정보", {
+            'fields': ('email', 'username',)
+        
+        }),
+        ('가입 수정 날짜', {
+            'fields': ('created_at', 'updated_at',)
+        }),
+    )
+    inlines = [UserProfileInline, PetProfileInline,]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserFollowing)
