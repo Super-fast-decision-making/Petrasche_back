d62a63498b6e743eb3cbc5117bb84c57c7b24a93
joohuun <k00005781@gmail.com>
2022-07-18 20:06:27 +0900

    edit 아티클시리얼라이즈

--- dm/admin.py
+++ dm/admin.py
@@ -3,5 +3,16 @@ from dm.models import Message, Header
 # Register your models here.
 
 admin.site.register(Message)
-admin.site.register(Header)
+# admin.site.register(Header)
 
+class Message(admin.TabularInline):
+    model = Message
+    
+    
+class HeaderAdmin(admin.ModelAdmin):
+    inlines = [Message]
+    class Meta:
+        model = Header
+
+
+admin.site.register(Header, HeaderAdmin)
