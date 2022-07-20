e035fd7bf4cdca1fb13e752929e898d59e743dea
SG-Marco <rollingpoe@gmail.com>
2022-07-20 18:24:26 +0900

    fix - Delete bulk data import

--- search_app/setting_bulk.py
+++ search_app/setting_bulk.py
@@ -1,10 +1,11 @@
 import requests, json, os
 from elasticsearch import Elasticsearch, exceptions
 
+
+es_url = 'http://localhost:9200'
 directory_path = 'path'
-res = requests.get('http://localhost:9200')
-# es = Elasticsearch([{'host':'localhost','port':'49153'}])
-es = Elasticsearch('http://localhost:9200')
+res = requests.get(es_url)
+es = Elasticsearch(es_url)
 
 try:
     es.indices.create(
@@ -21,25 +22,7 @@ try:
                     }
                 }
             }
-        },
-        # Elasticsearch의 인덱스에 들어가는 데이터의 타입을 정의 
-        # 설정해준 분석기 ‘my_analyzer’로 Restaurant과 Review를 분석할 수 있도록 설정
-        # mappings = {
-        #     "properties": {
-        #         "Restaurant": {
-        #             "type": "text",
-        #             "analyzer": "my_analyzer"
-        #         },
-        #         "Review": {
-        #             "type": "text",
-        #             "analyzer": "my_analyzer"
-        #         },
-        #         "id": {
-        #             "type": "long"
-        #         }
-        #     }
-        # }
-        
+        }
     )
 
 except exceptions.RequestError as ex:
@@ -47,27 +30,3 @@ except exceptions.RequestError as ex:
             pass # Index already exists. Ignore.
         else: # Other exception - raise it
             raise ex
-
-
-directory_path = 'C:\\Users\\SG\\Desktop\\sparta-SFDM\\Petrasche\\back\\search_app\\'
-with open(directory_path + 'article-new.json', encoding='utf-8') as json_file:
-    json_data = json.loads(json_file.read())
-
-body = ""
-count = 1
-for i in json_data:
-    del i['model']
-    body = body + json.dumps({"index": {"_index": "article", "_id": count}}) + '\n'
-    body = body + json.dumps(i, ensure_ascii=False) + '\n'
-    if count == 1:
-        print(body)
-    count += 1
-
-f = open(directory_path+'article.json', 'w')
-f.write(body)
-f.close()
-
-es.bulk(body=body)
-
-
-
