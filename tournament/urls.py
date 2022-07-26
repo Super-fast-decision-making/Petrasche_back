f99c00a438071f4b4991dffec0e5830d96ccd8b4
minmincoding <psjlmk@gmail.com>
2022-07-27 01:02:40 +0900

    fix 토너먼트 로직 수정

--- tournament/urls.py
+++ tournament/urls.py
@@ -5,5 +5,6 @@ from tournament import views
 urlpatterns = [
     path('', views.TournamentAttendantView.as_view()),
     path('period/', views.PetEventPeriodView.as_view()),
+    path('period/<int:pk>/', views.PetEventPeriodDetailView.as_view()),
     path('<int:pk>/', views.TournamentAttendantView.as_view()),
 ]
