2851548c0ba42bae67c9793b283ea6358f93cdc8
SG-Marco <rollingpoe@gmail.com>
2022-07-28 14:03:03 +0900

    초성검색 시도중

--- search_app/setting_bulk.py
+++ search_app/setting_bulk.py
@@ -7,10 +7,61 @@ directory_path = 'path'
 res = requests.get(es_url)
 es = Elasticsearch(es_url)
 
+
+
 try:
     es.indices.create(
         index='article',
-            # 한글 형태소 분석기 nori를 통해 데이터를 토크나이징할 수 있도록 설정
+        settings = {
+            "index": {
+            "analysis": {
+                "filter": {
+                "suggest_filter": {
+                    "type": "edge_ngram",
+                    "min_gram": 1,
+                    "max_gram": 50
+                }
+                },
+                "tokenizer": {
+                "jaso_search_tokenizer": {
+                    "type": "jaso_tokenizer",
+                    "mistype": "true",
+                    "chosung": "false"
+                },
+                "jaso_index_tokenizer": {
+                    "type": "jaso_tokenizer",
+                    "mistype": "true",
+                    "chosung": "true"
+                }
+                },
+                "analyzer": {
+                "suggest_search_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "jaso_search_tokenizer"
+                },
+                "suggest_index_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "jaso_index_tokenizer",
+                    "filter": [
+                    "suggest_filter"
+                    ]
+                }
+                }
+            }
+            }
+        }        
+)
+
+except exceptions.RequestError as ex:
+    print(80)
+    if ex.error == 'resource_already_exists_exception':
+        pass # Index already exists. Ignore.
+    else: # Other exception - raise it
+        raise ex
+
+try:
+    es.indices.create(
+        index='hashtag',
         settings = {
             "index": {
                 "analysis": {
@@ -26,7 +77,9 @@ try:
     )
 
 except exceptions.RequestError as ex:
-        if ex.error == 'resource_already_exists_exception':
-            pass # Index already exists. Ignore.
-        else: # Other exception - raise it
-            raise ex
+    print(28)
+    if ex.error == 'resource_already_exists_exception':
+        pass # Index already exists. Ignore.
+    else: # Other exception - raise it
+        raise ex
+
