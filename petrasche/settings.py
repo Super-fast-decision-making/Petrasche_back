700018b4b20a736258a03b94125d59af15f66028
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-01 02:14:38 +0900

    세팅 allowed_hosts에 backend 추가

--- petrasche/settings.py
+++ petrasche/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend']
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
